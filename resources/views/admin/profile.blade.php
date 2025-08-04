7ece32ec0a4007a4166e12f972b9720b8ec3c24a
Dahlia <hdieu12706@gmail.com>
2025-08-04 17:16:21 +0700

    Ngày 04/08 - Sửa giao diện

--- resources/views/admin/profile.blade.php
+++ resources/views/admin/profile.blade.php
@@ -163,7 +163,7 @@
                     <i class="fas fa-edit mr-2"></i>
                     Sửa thông tin
                 </a>
-                <a href="{{ route('dashboard') }}" class="btn btn-light-primary font-weight-bolder font-size-h6 px-8 py-4 my-3 mr-4">
+                <a href="{{ route('users.index') }}" class="btn btn-light-primary font-weight-bolder font-size-h6 px-8 py-4 my-3 mr-4">
                     <i class="fas fa-arrow-left mr-2"></i>
                     Quay lại Dashboard
                 </a>
