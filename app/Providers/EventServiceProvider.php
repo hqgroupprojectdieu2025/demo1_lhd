da02e806c0ab86667a95433435da5e6db4020f1f
Dahlia <hdieu12706@gmail.com>
2025-08-06 11:29:33 +0700

    Ngày 06/08 - Event->Mail

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use App\Events\UserRegistered;
+use App\Events\ForgotPassword;
 use App\Listeners\SendVerificationEmail;
+use App\Listeners\SendForgotEmail;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
@@ -11,6 +13,9 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         UserRegistered::class => [
             SendVerificationEmail::class,
+        ],
+        ForgotPassword::class => [
+            SendForgotEmail::class,
         ],
     ];
 
